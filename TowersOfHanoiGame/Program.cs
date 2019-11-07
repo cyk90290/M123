1a9ee85d2ad09eee1876ae61729251ac05067f57
cg2lab <cg2lab@DESKTOP-HCES3GB>
2019-11-07 21:05:07 +0800

    加入專案檔案。

---
+++ TowersOfHanoiGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoiGame
+{
+    class Program
+    {
+#pragma warning disable IDE0060 // 移除未使用的參數
+        static void Main(string[] args)
+#pragma warning restore IDE0060 // 移除未使用的參數
+        {
+            HanoiGame game = new HanoiGame();
+            game.Setup();
+            game.Play();
+
+            Console.ReadKey();
+        }
+    }
+}
